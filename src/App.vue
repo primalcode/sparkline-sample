19ec7ffca587a58a44f31e1d4954a92388521346
yoshima <yoshima.fukuda@codeberry.co.jp>
2023-12-28 18:38:28 +0900

    一旦ベースを

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,18 @@
 <template>
+  <v-card>
+    <v-card-title>My Bar Chart</v-card-title>
+    <v-card-text>
+      <BarChart />
+    </v-card-text>
+  </v-card>
 </template>
 
 <script>
+import BarChart from '@/components/BarChart.vue';
+
+export default {
+  components: {
+    BarChart
+  }
+}
 </script>
---
+++ src/components/Home.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>
+    <BarChart />
+  </div>
+</template>
+
+<script setup>
+import BarChart from '@/components/BarChart.vue';
+</script>
